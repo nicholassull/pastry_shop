fbccabd419eb514c7ac7b66da99115e43b672d96
Nick Sullivan <nicholassull@gmail.com>
2022-02-12 13:04:34 -0800

    add initial welcome text

--- PastryShop/Program.cs
+++ PastryShop/Program.cs
@@ -1,13 +1,27 @@
 using System;
-using System.Collections.Generic;
+using PastryShop.Models;
 
-namespace PrimeSifter
+
+namespace PastryShop
 {
   public class Program
   {
     public static void Main()
     {
-      
+      Console.WriteLine("Welcome to Pierre's Bakey online ording service!");
+      Console.WriteLine("=====================");
+      Console.WriteLine("We sell loafs of bread and pastries. In fact, we're having a huge sale right now!");
+      Console.WriteLine("Here are our current prices...");
+      Console.WriteLine("=====================");
+      Console.WriteLine("== Bread ==");
+      Console.WriteLine("1 Loaf: $5");
+      Console.WriteLine("3 Loaves: $10");
+      Console.WriteLine("== Pastries ==");
+      Console.WriteLine("1 Pastry: $2");
+      Console.WriteLine("3 Pastry: $5");
+      Console.WriteLine("4 Pastry: $7");
+      Console.WriteLine("5 Pastry: $9");
+      Console.WriteLine("6 Pastry: $10");
     }
 
   }
